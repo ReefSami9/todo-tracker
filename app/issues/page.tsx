1df43bdcbbbc8df73502c8fce065496946ade8c2
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-03 11:18:06 +0300

    Style navbar active links

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+      return (
+            <div>
+                  IssuesPAge
+            </div>
+      )
+}
+
+export default IssuesPage
