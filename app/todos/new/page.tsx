f916fb32d9f088560cfbcebf5370a9969ac8c853
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-08 12:55:20 +0300

    handle submit errors

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios'
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { InfoCircledIcon } from '@radix-ui/react-icons';
 
 interface TodoForm {
       title: string;
@@ -15,19 +17,33 @@ interface TodoForm {
 const NewTodoPage = () => {
       const router = useRouter();
       const { register, control, handleSubmit } = useForm<TodoForm>();
+      const [error, setError] = useState('');
       return (
-            <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
-                  await axios.post('/api/todos', data);
-                  router.push('/todos');
-            })}>
-                  <TextField.Root placeholder='Title' {...register('title')} />
-                  <Controller
-                        name="description"
-                        control={control}
-                        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
-                  />
-                  <Button>Submit</Button>
-            </form>
+            <div className='max-w-xl'>
+                  {error && <Callout.Root color='red' className='mb-5'>
+                        <Callout.Icon>
+                              <InfoCircledIcon />
+                        </Callout.Icon>
+                        <Callout.Text>{error}</Callout.Text>
+                  </Callout.Root>}
+                  <form className='space-y-3' onSubmit={handleSubmit(async (data) => {
+                        try {
+                              await axios.post('/api/todos', data);
+                              router.push('/todos');
+                        } catch (error) {
+                              setError('An unexpected error occurred.')
+                        }
+
+                  })}>
+                        <TextField.Root placeholder='Title' {...register('title')} />
+                        <Controller
+                              name="description"
+                              control={control}
+                              render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+                        />
+                        <Button>Submit</Button>
+                  </form>
+            </div >
       )
 }
 
