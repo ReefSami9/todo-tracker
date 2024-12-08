d43ec1a5152459c2753f4b5ab48c0b20eb18f312
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-08 12:39:11 +0300

    handle form submit

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,17 +1,33 @@
 'use client';
 
-import { Button, TextField, ThemePanel } from '@radix-ui/themes'
+import { Button, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios'
 import "easymde/dist/easymde.min.css";
-import React from 'react'
+import { useRouter } from 'next/navigation';
+
+interface TodoForm {
+      title: string;
+      description: string;
+}
 
 const NewTodoPage = () => {
+      const router = useRouter();
+      const { register, control, handleSubmit } = useForm<TodoForm>();
       return (
-            <div className='max-w-xl space-y-3'>
-                  <TextField.Root placeholder='Title' />
-                  <SimpleMDE placeholder='Description' />
+            <form className='max-w-xl space-y-3' onSubmit={handleSubmit(async (data) => {
+                  await axios.post('/api/todos', data);
+                  router.push('/todos');
+            })}>
+                  <TextField.Root placeholder='Title' {...register('title')} />
+                  <Controller
+                        name="description"
+                        control={control}
+                        render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+                  />
                   <Button>Submit</Button>
-            </div>
+            </form>
       )
 }
 
