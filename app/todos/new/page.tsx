414422ac3f1235be3a155a548253c889f94d1032
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-09 11:38:44 +0300

    add spinner

--- app/todos/new/page.tsx
+++ app/todos/new/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button, Callout, TextField, Text } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios'
@@ -11,6 +11,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
 import { createTodoSchema } from '@/app/validationsSchema';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 
 type TodoForm = z.infer<typeof createTodoSchema>;
@@ -21,6 +23,17 @@ const NewTodoPage = () => {
             resolver: zodResolver(createTodoSchema)
       });
       const [error, setError] = useState('');
+      const [isSubmitting, setSubmitting] = useState(false);
+      const onSubmit = handleSubmit(async (data) => {
+            try {
+                  setSubmitting(true);
+                  await axios.post('/api/todos', data);
+                  router.push('/todos');
+            } catch (error) {
+                  setSubmitting(false);
+                  setError('An unexpected error occurred.');
+            }
+      });
       return (
             <div className='max-w-xl'>
                   {error && <Callout.Root color='red' className='mb-5'>
@@ -29,24 +42,21 @@ const NewTodoPage = () => {
                         </Callout.Icon>
                         <Callout.Text>{error}</Callout.Text>
                   </Callout.Root>}
-                  <form className='space-y-3' onSubmit={handleSubmit(async (data) => {
-                        try {
-                              await axios.post('/api/todos', data);
-                              router.push('/todos');
-                        } catch (error) {
-                              setError('An unexpected error occurred.')
-                        }
-
-                  })}>
+                  <form className='space-y-3' onSubmit={onSubmit}>
                         <TextField.Root placeholder='Title' {...register('title')} />
-                        {errors.title && <Text color="red" as='p'>{errors.title.message}</Text>}
+                        <ErrorMessage>
+                              {errors.title?.message}
+                        </ErrorMessage>
+
                         <Controller
                               name="description"
                               control={control}
                               render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
                         />
-                        {errors.description && <Text color="red" as='p'>{errors.description.message}</Text>}
-                        <Button>Submit</Button>
+                        <ErrorMessage>
+                              {errors.description?.message}
+                        </ErrorMessage>
+                        <Button disabled={isSubmitting}>Submit {isSubmitting && <Spinner />}</Button>
                   </form>
             </div >
       )
