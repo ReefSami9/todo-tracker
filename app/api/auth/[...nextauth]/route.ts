05a7d4434c087a18b7925412d9bd5382baea82c4
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-17 14:15:39 +0300

    configure google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,13 @@
 import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
       providers: [
-            // OAuth authentication providers...
+            GoogleProvider({
+                  clientId: process.env.GOOGLE_CLIENT_ID!,
+                  clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+            })
+
       ],
 });
 
