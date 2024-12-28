e73da6c9deea9cd87c27a5bc0e40d4f17b9b6d47
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-28 12:38:30 +0300

    secure app

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/prisma/client';
+import { getServerSession } from "next-auth";
+import authOptions from "@/app/auth/authOptions";
 
 export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+      const session = await getServerSession(authOptions);
+      if (!session)
+            return NextResponse.json({}, { status: 401 })
+
       try {
             const body = await request.json();
             console.log('Request body:', body); // Log the incoming body
@@ -27,6 +33,10 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+      const session = await getServerSession(authOptions);
+      if (!session)
+            return NextResponse.json({}, { status: 401 })
+
       const todo = await prisma.todo.findUnique({
             where: { id: parseInt(params.id) },
       });
