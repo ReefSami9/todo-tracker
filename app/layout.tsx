ac0d208e71a13f9ff7bfd077cf4aa8ac60a6ef12
R.aljaithen <r.aljaithen@tamkeentech.sa>
2024-12-29 10:34:34 +0300

    query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,42 @@
 import "@radix-ui/themes/styles.css";
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './components/NavBar'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import NavBar from "./components/NavBar";
 import { Container, Theme } from "@radix-ui/themes";
 import AuthProvider from "./auth/Provider";
+import { useState } from "react";
+import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({
-  subsets: ['latin'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
-  title: 'ToDo | Track your Todos',
-  description: 'Track your Todos',
-}
+  title: "ToDo | Track your Todos",
+  description: "Track your Todos",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <AuthProvider>
-          <Theme accentColor="bronze" grayColor="mauve" radius="large">
-            <NavBar />
-            <main className="p-5 justify-center">
-              <Container>{children}</Container>
-            </main>
-          </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme accentColor="bronze" grayColor="mauve" radius="large">
+              <NavBar />
+              <main className="p-5 justify-center">
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
-  )
+  );
 }
